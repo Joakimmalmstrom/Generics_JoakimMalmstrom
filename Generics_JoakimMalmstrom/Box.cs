5e60b46d3749487811a7fc111d1b32b0608e898c
Joakim Malmström <35744406+Joakimmalmstrom@users.noreply.github.com>
2020-09-10 11:01:53 +0200

    Add project files.

---
+++ Generics_JoakimMalmstrom/Box.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Generics_JoakimMalmstrom
+{
+    internal class Box<T>
+    {
+        protected Stack<T> stack = new Stack<T>();
+
+        public int Count { get { return stack.Count; } }
+
+        public Box()
+        {
+        }
+
+        public void AddToStack(T element)
+        {
+            stack.Push(element);
+            Console.WriteLine($"{element} got pushed");
+        }
+
+
+        public T Remove()
+        {
+            return stack.Pop();
+        }
+    }
+}
---
+++ Generics_JoakimMalmstrom/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace Generics_JoakimMalmstrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var box = new Box<string>();
+                //var box = new Box<int>();
+
+                // Test with string
+                box.AddToStack("Daniel");
+                box.AddToStack("Johan");
+                box.AddToStack("Bertil");
+                box.AddToStack("Johanna");
+
+                // Test with int
+                //box.AddToStack(5);
+                //box.AddToStack(2);
+                //box.AddToStack(3);
+                //box.AddToStack(1);
+                //box.AddToStack(1);
+                //box.AddToStack(15);
+
+                Console.WriteLine($"The stack count is: {box.Count}");
+                Console.WriteLine($"Element {box.Remove()} removed from the stack");
+                Console.WriteLine($"The stack count is: {box.Count}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
